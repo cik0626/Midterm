58d91dafa378e83715bfb5b86a1e457ae31ef6a9
cik0626 <cik0626@naver.com>
2019-05-03 10:12:39 +0900

    중간고사

---
+++ src/main/java/poll/VoteMain.java
@@ -0,0 +1,19 @@
+package poll;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class VoteMain {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+				"vote-context.xml");
+		VoteService pollService = context.getBean("voteService",
+				VoteService.class);
+
+		pollService.voteFor();
+		pollService.listVotes();
+		context.close();
+	}
+
+}
